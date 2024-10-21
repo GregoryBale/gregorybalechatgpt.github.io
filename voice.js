51ab00e0d7971c4a2d95dca90269c3e698d4cc9d
GregoryBale <79316365+GregoryBale@users.noreply.github.com>
2024-10-21 11:34:06 +0300

    Delete voice.js

--- voice.js
+++
@@ -1,9 +0,0 @@
-  // Функция для открытия модального окна
-  function openModal() {
-    document.getElementById("recordModal").style.display = "block";
-  }
-
-  // Функция для закрытия модального окна
-  function closeModal() {
-    document.getElementById("recordModal").style.display = "none";
-  }
